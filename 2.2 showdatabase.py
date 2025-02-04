fb50af1a6a8fe38f2ce937b4ff6ae28a3242ed6d
azzhpsptsri25 <azizahpss25@gmail.com>
2025-02-05 00:26:09 +0800

    first commit

---
+++ 2.2 showdatabase.py
@@ -0,0 +1,14 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='',
+    database="db_penjualan"
+)
+
+mycursor = mydb.cursor()
+mycursor.execute('SHOW DATABASE')
+
+for x in mycursor:
+    print(x)
